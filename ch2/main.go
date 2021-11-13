e5995a01f59755c12742262606c09aefbcf550e0
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-11-13 15:23:15 +0900

    add sub

--- ch2/main.go
+++ ch2/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	claster1 :=  [[3,0], [4,3], [6,4], [7,1]]
+	claster2 := [[3,0], [4,3], [6,4], [7,1]]
+	for i := range claster1{
+		v := claster1[i] + claster2[i]
+		fmt.Println(v)
+	}
 }
